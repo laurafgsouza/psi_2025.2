cc8c767b0880db640d06a29e46458bfb397d4203
laurafgsouza <160539371+laurafgsouza@users.noreply.github.com>
2025-09-16 20:38:22 +0000

    inserindo paginas html e criando links entre elas

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'hellooo world!'
+  return 'Essa é minha primeira aplicação em Flask!'
 
 @app.route('/contato')
 def contato():
-    return 'gomes.laura@escolar.ifrn.edu.br'
+  return render_template('contato.html')
+
+@app.route('/exemplo')
+def exemplo():
+  return render_template('exemplo.html')
+
+@app.route('/exemplo2')
+def exemplo2():
+  return render_template('exemplo2.html')
 
 if __name__ == '__main__':
     app.run()
